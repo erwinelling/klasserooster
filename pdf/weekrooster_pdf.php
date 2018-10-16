17019501771a30254c437734227e36498f566b09
Erwin Elling <erwinelling@gmail.com>
2018-10-16 11:50:35 +0200

    Updated example url

--- pdf/weekrooster_pdf.php
+++ pdf/weekrooster_pdf.php
@@ -16,7 +16,7 @@ $options = array(
     'page-size' => 'A4',
 );
 
-$pdf = new Pdf("https://keesboeke.guifiontwikkelt.nl/weekroosterleerling_ee.php?output=clean&docent=Administrator&klas=1&sCode=JBR&ll[]=261");//&ll[]=287&ll[]=282&ll[]=264&ll[]=263&ll[]=278&ll[]=288&ll[]=284&ll[]=270&ll[]=286&ll[]=276&ll[]=262&ll[]=285&ll[]=280&ll[]=268&ll[]=259&ll[]=266&ll[]=272&ll[]=269&ll[]=271&ll[]=265&ll[]=275&ll[]=274&ll[]=273&ll[]=260&ll[]=283&ll[]=281&ll[]=277&ll[]=279&ll[]=267");
+$pdf = new Pdf("https://keesboeke.guifiontwikkelt.nl/weekroosterleerling_new.php?output=clean&docent=Administrator&klas=1&sCode=JBR&week=41&ll[]=261&ll[]=287&ll[]=282&ll[]=264&ll[]=263&ll[]=278&ll[]=288&ll[]=284&ll[]=270&ll[]=286&ll[]=276");//&ll[]=262&ll[]=285&ll[]=280&ll[]=268&ll[]=259&ll[]=266&ll[]=272&ll[]=269&ll[]=271&ll[]=265&ll[]=275&ll[]=274&ll[]=273&ll[]=260&ll[]=283&ll[]=281&ll[]=277&ll[]=279&ll[]=267");
 $pdf->setOptions($options);
 
 if (!$pdf->saveAs('/data/sites/web/klasseroosternl/subsites/klasserooster.nl/pdf/rooster.pdf')) {
